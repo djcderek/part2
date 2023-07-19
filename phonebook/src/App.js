af6c599b62d5722649cc3a18cdaa8e7c7ffb16a2
Derek Chan <djcderek@gmail.com>
2023-07-19 14:48:11 -0700

    complete exercise 2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,15 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3},
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
   ]) 
+  const [filteredPersons, setFilteredPersons] = useState(persons)
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addNumber = (personObject) => {
     personObject.number = newNumber
-    console.log(personObject)
-    console.log('number added')
+    //console.log(personObject)
+    //console.log('number added')
   }
 
   const addPerson = (event) => {
@@ -22,20 +28,20 @@ const App = () => {
       alert(`${newName} is already added to the phonebook`)
       return
     }
-    addNumber(personObject)
 
-    setPersons(persons.concat(personObject))
-    
-    console.log(persons)
+    addNumber(personObject)
+    const tempPersons = [...persons]
+    setPersons(tempPersons.concat(personObject))
+    setFilteredPersons(tempPersons.concat(personObject))
   }
 
   const handlePersonChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
@@ -43,9 +49,23 @@ const App = () => {
     return persons.some(a => a.name === person)
   }
 
+  const search = (event) => {
+    const currValue = event.target.value
+    setNewFilter(currValue)
+
+    const tempPeople = [...persons]
+    const filteredPeople = tempPeople.filter(person => {
+      return person.name.toLowerCase().includes(currValue.toLowerCase())
+    })
+    setFilteredPersons(filteredPeople)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={newFilter} onChange={search}></input>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
@@ -57,7 +77,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}</div>
+      <div>{filteredPersons.map(person => {
+        return <div key={person.name}>{person.name} {person.number}</div>
+        // if (person.visible) {
+        //   return <div key={person.name}>{person.name} {person.number}</div>
+        // }
+      })}</div>
     </div>
   )
 }
