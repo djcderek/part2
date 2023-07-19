efa0e58fb2c684204357146a74eb39d34d74670a
Derek Chan <djcderek@gmail.com>
2023-07-18 21:09:54 -0700

    Move more things

--- src/components/Part.js
+++
@@ -1,8 +0,0 @@
-const Part = (props) => {
-    console.log('entered')    
-    return (
-        <div>{props.name} {props.exercises}</div>
-    )
-}
-
-export default Part
--- src/components/Total.js
+++
@@ -1,9 +0,0 @@
-const Total = (props) => {
-    const course = props.course
-    console.log('from total', course)
-    return (
-        <h4>total of {course.reduce((a, b) => a + b.exercises, 0)} exercises</h4>
-    )
-}
-
-export default Total
