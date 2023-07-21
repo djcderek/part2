5626e14d999b57a6bd9f1d972ca0beaa2583f7be
Derek Chan <djcderek@gmail.com>
2023-07-21 00:54:12 -0700

    Complete exercise 2.11

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,20 +1,27 @@
 import { useState } from 'react'
+import { useEffect} from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Display from './components/Display'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3},
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log(response.data)
+      setPersons(response.data)
+      setFilteredPersons(response.data)
+    })
+  }
+
+  useEffect(hook, [])
 
   const addNumber = (personObject) => {
     personObject.number = newNumber
